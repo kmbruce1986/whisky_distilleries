194d2bd7aa2ca49a0d7e252f71d3d09fbcc5ecc6
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-10 16:38:19 +0100

    adds get distillery by region method

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -1,4 +1,50 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+//
+//    @Override
+//    @Transactional
+//    public List<Whisky> getWhiskiesByRegion(String region){
+//        List<Whisky> results = null;
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        Criteria cr = session.createCriteria(Whisky.class);
+//        cr.createAlias("distillery")
+//
+//    }
+
+    @Override
+    @Transactional
+    public List<Distillery> findDistilleriesByRegion(String region){
+        List<Distillery> results = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria cr = session.createCriteria(Distillery.class);
+            cr.add(Restrictions.eq("region", region));
+            results = cr.list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return results;
+    }
+
 }
